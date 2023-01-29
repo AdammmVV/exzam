9a3a5bc71b67297c62c374c46607c051f6360ffa
Adammm <adammm.web@gmail.com>
2023-01-30 02:01:55 +0300

    change App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {CounterMain} from "./components/counter/CounterMain";
-import {Telegramm} from "./components/TG/Telegramm";
+import {MainCounter} from "./components/counter/Counter/MainCounter";
+import {SettingsMainCounter} from "./components/counter/SettingsMainCounter";
 
 export const App = () => {
+
+    const [maxValue, setMaxValue] = useState<number>(5)
+    const [minValue, setMinValue] = useState<number>(0)
+
+    const changeMaxValue = (max:number) => {
+        setMaxValue(max)
+    }
+    const changeMinValue = (min:number) => {
+        setMinValue(min)
+    }
+
     return (
-        <div>
-            <CounterMain/>
-            <Telegramm/>
+        <div className={'appWrapper'}>
+            <SettingsMainCounter changeMaxValue={changeMaxValue}
+                                 changeMinValue={changeMinValue}/>
+            <MainCounter maxValue={maxValue} minValue={minValue}/>
+            {/*<Telegramm/>*/}
         </div>
     )
 }
