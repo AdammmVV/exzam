8ba4b61bb2264294d4e804b61503ab17e3f15b45
Adammm <adammm.web@gmail.com>
2023-01-30 15:36:12 +0300

    change Counter.tsx

--- src/components/counter/Counter/Counter.tsx
+++ src/components/counter/Counter/Counter.tsx
@@ -2,12 +2,20 @@ import React from "react";
 
 type CounterPropsType = {
     counter: number
+    error: boolean | 'change'
     red: { color: string }
 }
 export const Counter = (props: CounterPropsType) => {
+
+    const finalValue = props.error === true
+        ? <div className={'incorrectValue'}>Incorrect value!</div>
+        : props.error === "change"
+            ? <div className={'setValues'}>enter values and press 'set'</div>
+            :  props.counter
+
     return (
         <div className='tablo' style={props.red}>
-            {props.counter}
+            {finalValue}
         </div>
     )
 }
