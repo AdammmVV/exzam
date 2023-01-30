44408908b2c80d20f93964dc599a705dc057cada
Adammm <adammm.web@gmail.com>
2023-01-30 15:36:25 +0300

    change MainCounter.tsx

--- src/components/counter/Counter/MainCounter.tsx
+++ src/components/counter/Counter/MainCounter.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {SuperButton} from "../SuperButton/SuperButton";
 import {Counter} from "./Counter";
 
@@ -6,22 +6,29 @@ import {Counter} from "./Counter";
 type MainCounterPropsType = {
     maxValue: number
     minValue: number
+    error: boolean | 'change'
 }
 
 export const MainCounter:React.FC<MainCounterPropsType> = (
     {
         maxValue,
-        minValue
+        minValue,
+        error,
+
     }
 ) => {
 
-    const [counter, setCounter] = useState<number>(minValue);
+    const [counter, setCounter] = useState<number>(0);
+
+    useEffect(()=> {
+        setCounter(minValue)
+    }, [minValue])
 
     const counterButtonInc = () => {
         setCounter(counter + 1)
     }
 
-    let red = counter === maxValue ? {color: 'red'} : {color: 'white'}
+    let red = counter === maxValue ? {color: 'red', fontSize: '60px'} : {color: 'white'}
 
     const counterButtonReset = () => {
         setCounter(minValue)
@@ -30,7 +37,7 @@ export const MainCounter:React.FC<MainCounterPropsType> = (
 
     return (
         <div className='counter'>
-            <Counter counter={counter} red={red}/>
+            <Counter counter={counter} red={red} error={error}/>
             <div className='button'>
                 <SuperButton callBack={counterButtonInc} name={'inc'} disabled={counter >= maxValue}/>
                 <SuperButton callBack={counterButtonReset} name={'reset'} disabled={counter === minValue}/>
