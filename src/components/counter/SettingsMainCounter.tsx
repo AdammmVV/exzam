42b28726de27c08951d7244f08d5b69140e301a1
Adammm <adammm.web@gmail.com>
2023-01-30 15:36:51 +0300

    change SettingsMainCounter.tsx

--- src/components/counter/SettingsMainCounter.tsx
+++ src/components/counter/SettingsMainCounter.tsx
@@ -3,22 +3,26 @@ import {SuperButton} from "./SuperButton/SuperButton";
 import {SettingCounter} from "./SettingCounter";
 
 type SettingsMainCounterPropsType = {
-    changeMaxValue: (max:number)=>void
-    changeMinValue: (min:number)=>void
+    changeMaxValue: (max: number) => void
+    changeMinValue: (min: number) => void
+    setError: (val: boolean | 'change') => void
 }
 
-export const SettingsMainCounter:React.FC<SettingsMainCounterPropsType> = (
+export const SettingsMainCounter: React.FC<SettingsMainCounterPropsType> = (
     {
         changeMaxValue,
         changeMinValue,
+        setError,
     }
 ) => {
     const [maxInputValue, setMaxInputValue] = useState<number>(5)
     const [minInputValue, setMinInputValue] = useState<number>(0)
 
+
     const changeSettingCounter = () => {
         changeMaxValue(maxInputValue)
         changeMinValue(minInputValue)
+        setError(false)
     }
 
     return (
@@ -26,9 +30,11 @@ export const SettingsMainCounter:React.FC<SettingsMainCounterPropsType> = (
             <SettingCounter maxInputValue={maxInputValue}
                             minInputValue={minInputValue}
                             setMaxInputValue={setMaxInputValue}
-                            setMinInputValue={setMinInputValue}/>
+                            setMinInputValue={setMinInputValue}
+                            setError={setError}/>
             <div className='button'>
-                <SuperButton callBack={changeSettingCounter} name={'set'} disabled={false}/>
+                <SuperButton callBack={changeSettingCounter} name={'set'}
+                             disabled={(maxInputValue <= minInputValue || minInputValue < 0)}/>
             </div>
         </div>
     );
