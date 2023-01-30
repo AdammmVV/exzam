ccf52c639a86d176b10b8dc3967ad346938a6d7c
Adammm <adammm.web@gmail.com>
2023-01-30 15:37:05 +0300

    create SuperInputNumber.tsx

---
+++ src/components/counter/SuperInputNumber.tsx
@@ -0,0 +1,21 @@
+import React, {ChangeEvent} from "react";
+
+type SuperInputNumberPropsType = {
+    changeInputValue: (value: number) => void
+    value: number
+    className: string
+}
+export const SuperInputNumber: React.FC<SuperInputNumberPropsType> = (
+    {
+        changeInputValue,
+        value,
+        className,
+    }
+) => {
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        changeInputValue(+e.currentTarget.value)
+    }
+    return (
+        <input onChange={onChangeHandler} type="number" value={value} className={className}/>
+    )
+}
