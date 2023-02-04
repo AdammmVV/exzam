8530a38452e40c77af7caf919b27e7bc9873fbe5
Adammm <adammm.web@gmail.com>
2023-02-04 21:11:22 +0300

    refactor SettingsCounter.tsx

--- src/components/counter/SettingCounter.tsx
+++ src/components/counter/SettingCounter.tsx
@@ -23,32 +23,23 @@ export const SettingCounter:React.FC<SettingCounterPropsType> = (
     }
 
 
-    const changeMaxInputValue = (value: number) => {
-        setMaxInputValue(value)
-        setError("change")
-    }
-
-    const changeMinInputValue = (value: number) => {
-        setMinInputValue(value)
-        setError("change")
-    }
 
     let finalStyle = `inputNumber${(maxInputValue <= minInputValue || minInputValue < 0) ? ' errorInput' : ''}`
 
     return (
         <div className={'settingWrapper'}>
-            <div className={'maxWrapper'}>
-                <span>max value:</span>
-                <SuperInputNumber changeInputValue={changeMaxInputValue}
-                                  value={maxInputValue}
-                                  className={finalStyle}/>
-            </div>
-            <div className={'maxWrapper'}>
-                <span>start value:</span>
-                <SuperInputNumber changeInputValue={changeMinInputValue}
-                                  value={minInputValue}
-                                  className={finalStyle}/>
-            </div>
+            {/*<div className={'maxWrapper'}>*/}
+            {/*    <span>max value:</span>*/}
+            {/*    <SuperInputNumber changeInputValue={changeMaxInputValue}*/}
+            {/*                      value={maxInputValue}*/}
+            {/*                      className={finalStyle}/>*/}
+            {/*</div>*/}
+            {/*<div className={'maxWrapper'}>*/}
+            {/*    <span>start value:</span>*/}
+            {/*    <SuperInputNumber changeInputValue={changeMinInputValue}*/}
+            {/*                      value={minInputValue}*/}
+            {/*                      className={finalStyle}/>*/}
+            {/*</div>*/}
         </div>
     )
 }
