078259527e6a4751762309059cf1a9691d6cb053
Adammm <adammm.web@gmail.com>
2023-02-04 21:10:28 +0300

    refactor Counter.tsx

--- src/components/counter/Counter/Counter.tsx
+++ src/components/counter/Counter/Counter.tsx
@@ -1,20 +1,26 @@
 import React from "react";
+import {StatusType} from "../../../App";
 
 type CounterPropsType = {
     counter: number
-    error: boolean | 'change'
+    status: StatusType
     red: { color: string }
 }
-export const Counter = (props: CounterPropsType) => {
+export const Counter: React.FC<CounterPropsType> = (
+    {
+        counter,
+        status,
+        red,
+    }) => {
 
-    const finalValue = props.error === true
+    const finalValue = status === 'error'
         ? <div className={'incorrectValue'}>Incorrect value!</div>
-        : props.error === "change"
+        : status === 'changed'
             ? <div className={'setValues'}>enter values and press 'set'</div>
-            :  props.counter
+            : counter
 
     return (
-        <div className='tablo' style={props.red}>
+        <div className='tablo' style={red}>
             {finalValue}
         </div>
     )
