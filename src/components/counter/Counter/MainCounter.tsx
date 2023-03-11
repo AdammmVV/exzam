acd57205ac3eaa424aedce58ba7f1f2d06fb441b
Adammm <adammm.web@gmail.com>
2023-03-12 02:30:01 +0300

    refactor MainCounter.tsx for REDUX

--- src/components/counter/Counter/MainCounter.tsx
+++ src/components/counter/Counter/MainCounter.tsx
@@ -1,52 +1,37 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {SuperButton} from "../SuperButton/SuperButton";
 import {Counter} from "./Counter";
-import {StatusType} from "../../../App";
+import {useSelector} from "react-redux";
+import {AppRootType} from "../../../redux/store";
+import {CounterStateType} from "../../../redux/counterReducer";
 
+export const MainCounter: React.FC = () => {
 
-type MainCounterPropsType = {
-    maxValue: number
-    minValue: number
-    status: StatusType
-}
-
-export const MainCounter: React.FC<MainCounterPropsType> = (
-    {
-        maxValue,
-        minValue,
-        status,
-    }
-) => {
-
+    const counterState = useSelector<AppRootType, CounterStateType>(state => state.counterState)
     const [counter, setCounter] = useState<number>(0);
-
-    useEffect(() => {
-        setCounter(minValue)
-    }, [minValue])
 
     const counterButtonInc = () => {
         setCounter(counter + 1)
     }
 
-    let red = counter === maxValue ? {color: 'red', fontSize: '60px'} : {color: 'white'}
-
     const counterButtonReset = () => {
-        setCounter(minValue)
+        setCounter(counterState.startValue)
     }
+
+    let red = counter === counterState.maxValue ? {color: 'red', fontSize: '60px'} : {color: 'white'}
 
     return (
         <div className='counter'>
             <Counter counter={counter}
                      red={red}
-                     status={status}
             />
             <div className='button'>
                 <SuperButton callBack={counterButtonInc}
                              name={'inc'}
-                             disabled={counter >= maxValue}/>
+                             disabled={counter >= counterState.maxValue}/>
                 <SuperButton callBack={counterButtonReset}
                              name={'reset'}
-                             disabled={counter === minValue}/>
+                             disabled={counter === counterState.startValue}/>
             </div>
         </div>
     );
